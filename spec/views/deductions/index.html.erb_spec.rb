b0420e28dc510958c3a2bcfecd9b78b1c4d29235
Brad Rice <bradley_rice@mac.com>
2014-02-11 21:36:16 -0600

    Update test suite for new views

--- spec/views/deductions/index.html.erb_spec.rb
+++ spec/views/deductions/index.html.erb_spec.rb
@@ -21,6 +21,5 @@ describe "deductions/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 1.5.to_s, :count => 2
     assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
   end
 end
