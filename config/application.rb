9a80e3e7b636bf710ad48e725a30653ef1a597fb
Brad Rice <bradley_rice@mac.com>
2014-02-13 23:18:32 -0600

    Another fix for Heroku deployment. Problem with database connection

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,9 @@ module BetaBudget
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    # Heroku deployment bugfix
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures:           true,
