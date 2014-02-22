b483b05645042ae17954ef658d2a0512e27a7c30
Brad Rice <bradley_rice@mac.com>
2014-02-21 21:24:32 -0600

    Use new User instance method to assign @pay_period

--- app/controllers/pay_periods_controller.rb
+++ app/controllers/pay_periods_controller.rb
@@ -25,8 +25,7 @@ class PayPeriodsController < ApplicationController
   # GET /pay_periods/current
   # GET /pay_periods/current.json
   def current
-    @pay_period = current_user.pay_periods
-      .where("start_date <= '#{Date.today}' and end_date >= '#{Date.today}'").last
+    @pay_period = current_user.current_pay_period
     if @pay_period
       @withdrawal = Withdrawal.new(pay_period_id: @pay_period.id)
 
